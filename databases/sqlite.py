296b51d73c848a35e835b47a876158a543ff61b1
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2025-01-07 15:46:25 +0300

    доработка sqlite.py

--- databases/sqlite.py
+++ databases/sqlite.py
@@ -4,11 +4,17 @@ from sqlite3 import Cursor, connect
 class SQLite3Client:
     def __init__(self):
         self.connection, self.cursor = self.connect()
+        self.connection_working_chats, self.cursor_working_chats = self.connect_to_working_chats_db()
         self.create_table()
 
     @staticmethod
     def connect():
         connection = connect('tg.sql')
+        return connection, connection.cursor()
+
+    @staticmethod
+    def connect_to_working_chats_db():
+        connection = connect('working_chats.sql')
         return connection, connection.cursor()
 
     def create_table(self):
@@ -17,7 +23,8 @@ class SQLite3Client:
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS pid_table(pid INT UNSIGNED)""")
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS admin_chats(chat_id INT, PRIMARY KEY(chat_id))""")
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS ban_users(nickname TEXT, id INT, PRIMARY KEY(nickname))""")
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS working_chat(url TEXT, id_of_message INT, message TEXT, check_completely BOOL, PRIMARY KEY(url))""")
+        self.cursor_working_chats.execute("""CREATE TABLE IF NOT EXISTS working_chat(url TEXT, id_of_message INT, message TEXT, check_completely BOOL, PRIMARY KEY(url))""")
+        self.connection_working_chats.commit()
 
         self.connection.commit()
 
@@ -97,42 +104,42 @@ class SQLite3Client:
 
     def add_working_chat_into_table(self, url: str, id_of_message: int = 0, message: str = '', check_completely: bool = 0):
         try:
-            self.cursor.execute("""INSERT INTO working_chat(url, id_of_message, message, check_completely) VALUES (?, ?, ?, ?)"""
-                                , (url, id_of_message, message, check_completely))
-            self.connection.commit()
+            self.cursor_working_chats.execute("""INSERT INTO working_chat(url, id_of_message, message, check_completely) VALUES (?, ?, ?, ?)"""
+                                ,(url, id_of_message, message, check_completely))
+            self.connection_working_chats.commit()
         except Exception:
             pass
 
     def update_id_of_message(self, url: str, id_of_message: int):
-        self.cursor.execute(f"""UPDATE working_chat SET id_of_message = {id_of_message} WHERE url = "{url}" """)
-        self.connection.commit()
+        self.cursor_working_chats.execute(f"""UPDATE working_chat SET id_of_message = {id_of_message} WHERE url = "{url}" """)
+        self.connection_working_chats.commit()
 
     def update_message(self, url: str, message: str):
-        self.cursor.execute(f"""UPDATE working_chat SET message = "{message}" WHERE url = "{url}" """)
-        self.connection.commit()
+        self.cursor_working_chats.execute(f"""UPDATE working_chat SET message = "{message}" WHERE url = "{url}" """)
+        self.connection_working_chats.commit()
 
     def update_check_completely(self, url: str):
-        self.cursor.execute(f"""UPDATE working_chat SET check_completely = 1 WHERE url = "{url}" """)
-        self.connection.commit()
+        self.cursor_working_chats.execute(f"""UPDATE working_chat SET check_completely = 1 WHERE url = "{url}" """)
+        self.connection_working_chats.commit()
 
     def setup_working_chats(self, url_of_chats: list[str]):
         for url in url_of_chats:
             self.add_working_chat_into_table(url)
 
     def get_id_of_message(self, url: str):
-        self.cursor.execute(f"""SELECT id_of_message FROM working_chat WHERE url = "{url}" """)
-        return self.cursor.fetchall()[0][0]
+        self.cursor_working_chats.execute(f"""SELECT id_of_message FROM working_chat WHERE url = "{url}" """)
+        return self.cursor_working_chats.fetchall()[0][0]
 
     def get_message(self, url: str):
         try:
-            self.cursor.execute(f"""SELECT message FROM working_chat WHERE url = "{url}" """)
-            return self.cursor.fetchall()[0][0]
+            self.cursor_working_chats.execute(f"""SELECT message FROM working_chat WHERE url = "{url}" """)
+            return self.cursor_working_chats.fetchall()[0][0]
         except IndexError:
             return ''
 
     def get_check_completely(self, url: str):
-        self.cursor.execute(f"""SELECT check_completely FROM working_chat WHERE url = "{url}" """)
-        return self.cursor.fetchall()[0][0]
+        self.cursor_working_chats.execute(f"""SELECT check_completely FROM working_chat WHERE url = "{url}" """)
+        return self.cursor_working_chats.fetchall()[0][0]
 
 
 sqlite3_client = SQLite3Client()
