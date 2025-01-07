8e78628a14bbfa1b368e2644033726fb46ab9325
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2025-01-07 03:35:15 +0300

    доработка check_message.py

--- src/telethon_interface/check_message.py
+++ src/telethon_interface/check_message.py
@@ -6,7 +6,7 @@ def check_message(text: str):
     try:
         key_words = sqlite3_client.get_key_words()
         for key_word in key_words:
-            if re.findall(key_word[0], text.lower().replace('\n', ' ').strip()):
+            if len(re.findall(key_word[0].lower(), text.lower().replace('\n', ' ').strip())) > 0:
                 return True
         return False
     except Exception:
